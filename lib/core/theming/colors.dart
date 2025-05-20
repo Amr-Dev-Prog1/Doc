ab928c71d156e2c872b867c8b963de27c0152446
AMR-dev-prog <araed2178@gmail.com>
2025-05-21 00:03:15 +0300

    -added spacing file for SizedBox -Added some colors and textstyles -added FontWiaghtHelber -add Genral TextButton & genral TextFormFiled -Finsh LoginScreen desing

--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -4,8 +4,11 @@ import 'dart:ui';
 class ColorsManger {
   
 
-  static const Color mainBlue = Color(0xFF247CFF);
+    static const Color mainBlue = Color(0xFF247CFF);
   static const Color gray = Color(0xFF757575);
+  static const Color ligtgray = Color(0xFFC2C2C2);
+  static const Color ligtergray = Color(0xFFEDEDED);
+  static const Color moreligtgray = Color(0xFFfdfdff);
   static const Color textColor = Color(0xFF000000);
   static const Color errorColor = Color(0xFFB00020);
 }
--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/theming/colors.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TextStyles {
@@ -7,19 +8,56 @@ class TextStyles {
     fontWeight: FontWeight.w700,
     color: Colors.black,
   );
+
+  static final TextStyle font24BlueBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.bold,
+    color: Colors.blue,
+  );
+
   static final TextStyle font32BlueBold = TextStyle(
     fontSize: 32.sp,
     fontWeight: FontWeight.bold,
     color: Colors.blue,
   );
 
-  static final TextStyle font13GrayRegular = TextStyle(
+  static final TextStyle font15GrayNormal = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: FontWeight.normal,
+    color: Colors.grey,
+  );
+
+  static final TextStyle font13BlueNormal = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.normal,
+    color: Colors.blue,
+  );
+
+  static final TextStyle font16BlueSemiBold = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+    color: Colors.blue,
+  );
+
+  static final TextStyle font13GrayNormal = TextStyle(
     fontSize: 13.sp,
     fontWeight: FontWeight.normal,
     color: Colors.grey,
   );
 
-  static final TextStyle font16WhitSiemiBold = TextStyle(
+  static final TextStyle font13DarkBlueMedium = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w500,
+    color: Colors.blue[900],
+  );
+
+  static final TextStyle font16DarkBlueMedium = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+    color: Colors.blue[900],
+  );
+
+  static final TextStyle font16WhiteSemiBold = TextStyle(
     fontSize: 16.sp,
     fontWeight: FontWeight.w500,
     color: Colors.white,
@@ -28,6 +66,18 @@ class TextStyles {
   static final TextStyle font14Black45_400Weight = TextStyle(
     fontSize: 14.sp,
     fontWeight: FontWeight.w400,
-    color: Colors.black45,
+    color: ColorsManger.textColor,
   );
+static final TextStyle font14lightGrayNormal = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.normal,
+    color: ColorsManger.ligtgray,
+  );
+
+  static var font13GrayRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w400,
+    color: ColorsManger.gray,
+  );
+  
 }
